c0698b5129f73d197f22ae9dd646d3e28af46115
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-04-20 10:03:40 +0000

    version final.

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector("#expenses-list");
+const totalExpensesOutput = document.querySelector("#total-expenses");
+const alertCtrl = document.querySelector('ion-alert-controller');
+
+let totalExpenses =  0;
+
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+}
 
 confirmBtn.addEventListener('click', () => {
     const entredReason = reasonInput.value;
@@ -12,9 +21,19 @@ confirmBtn.addEventListener('click', () => {
         entredAmount <= 0 ||
         entredAmount.trim().length <= 0
     ){
+        alertCtrl.create({
+            message: 'Please enter a valid reason and amount !',
+            header: 'Invalid inputs',
+            buttons: ['Okay']
+        }).then(alertElement => {alertElement.present();});
         return ;
     }
     const newItem = document.createElement('ion-item');
     newItem.textContent = entredReason + ': $' + entredAmount;
     expensesList.appendChild(newItem);
+    totalExpenses+= +entredAmount ;
+    totalExpensesOutput.textContent = totalExpenses
+    clear();
 })
+
+cancelBtn.addEventListener('click', clear)
